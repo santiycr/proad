8bb8411b4301f9dd37f7bd51f4a0836545a5289e
santi <santiycr@gmail.com>
2011-08-27 21:33:25 -0300

    valores por defecto de tareas

--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -1,11 +1,19 @@
 class Tarea < ActiveRecord::Base
-    belongs_to :userstory
-    validates_presence_of :userstory
-    validates_numericality_of :porcentaje_avance,
-      :only_integer => true,
-      :less_than_or_equal_to => 100,
-      :message => "debe ser inferior a 100"
-    validates_numericality_of :horas_dedicadas,
-      :only_integer => true,
-      :message => "debe ser un numero"
+  belongs_to :userstory
+  validates_presence_of :userstory
+  validates_numericality_of :porcentaje_avance,
+    :only_integer => true,
+    :less_than_or_equal_to => 100,
+    :message => "debe ser inferior a 100"
+  validates_numericality_of :horas_dedicadas,
+    :only_integer => true,
+    :message => "debe ser un numero"
+
+  after_initialize :init
+
+  def init
+   self.horas_dedicadas ||= 0
+   self.porcentaje_avance ||= 0
+  end
+
 end
